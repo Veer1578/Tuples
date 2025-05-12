d24d86c2a86af096205ee05b4a42564e618116ec
Veer1578 <sweetyatulshah@gmail.com>
2025-05-12 09:14:11 +0530

    operatios

---
+++ operatios.py
@@ -0,0 +1,22 @@
+#tuple with different data types
+data_types = ('tuple', False, 3.2, 5)
+print(data_types)
+
+#create tuple
+tuple1 = (4, 3, 2, 6, 5, 7)
+print(tuple1)
+
+#merging tuples using +
+tuple1 = tuple1 + (9,)
+print(tuple1)
+
+#counting occurence
+occur = (10, 20, 30, 70, 50, 50)
+print(occur.count(50))
+
+tuplex = (4, 3, 3, 2, 8, 7, 8, 5, 9)
+_slice = tuplex[3:5]
+print(_slice)
+
+_slice = tuplex[:6]
+print(_slice)
